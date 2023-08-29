04e48bd1cce7af38a6e7b6e45e201c945dce7be5
Abdelkarim ouazmir <ouazmir@gmail.com>
2023-08-29 16:14:15 -0700

    0x1D. C - Binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,16 +9,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+	binary_tree_t *_n_v;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
+	_n_v = malloc(sizeof(binary_tree_t));
+	if (_n_v == NULL)
 		return (NULL);
 
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+	_n_v->n = value;
+	_n_v->parent = parent;
+	_n_v->left = NULL;
+	_n_v->right = NULL;
 
-	return (new);
+	return (_n_v);
 }
